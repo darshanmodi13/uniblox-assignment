101142d336d9217f29a0335dad0e102c50137263
Darshan Modi <darshan@stockunlock.com>
2025-12-27 22:18:52 +0530

    Backend changes for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import app from '~/app';
 
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, () => {
-	console.log(`🚀 Server running on http://localhost:${PORT}`);
+	console.log(`🚀 Server running on http://${HOST}:${PORT}`);
 });
